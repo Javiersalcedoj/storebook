26b9ec1606e0fb1a2beab231146edff1cf03f1c5
Javier Salcedo <javierss.2603@gmail.com>
2021-10-23 02:36:26 -0500

    fix bug imgen

--- src/books.js
+++ src/books.js
@@ -14,17 +14,17 @@ class Book {
     }
 }
 
-const bookAprendeIA = new Book ({name:'Aprende IA', author: 'unknown', description: 'here is a description', img: '../img/Portada.png'});
-const bookProgramar = new Book ({name:'Programar', author: 'unknown', description: 'here is a description', img: '../img/Portada.png'});
-const bookJavascript = new Book ({name:'JavaScript', author: 'unknown', description: 'here is a description', img: '../img/Portada.png'});
+const bookAprendeIA = new Book ({name:'Aprende IA', author: 'unknown', description: 'here is a description', img: './img/Portada.png'});
+const bookProgramar = new Book ({name:'Programar', author: 'unknown', description: 'here is a description', img: './img/Portada.png'});
+const bookJavascript = new Book ({name:'JavaScript', author: 'unknown', description: 'here is a description', img: './img/Portada.png'});
 categoryTecnology.addRecomendation([bookAprendeIA, bookProgramar, bookJavascript]);
 
-const bookInvierte = new Book ({name:'Invierte', author: 'unknown', description: 'here is a description', img: '../img/Portada.png'})
-const bookFinanzas = new Book ({name:'Finanzas', author: 'unknown', description: 'here is a description', img: '../img/Portada.png'})
+const bookInvierte = new Book ({name:'Invierte', author: 'unknown', description: 'here is a description', img: './img/Portada.png'})
+const bookFinanzas = new Book ({name:'Finanzas', author: 'unknown', description: 'here is a description', img: './img/Portada.png'})
 categoryFinance.addRecomendation([bookInvierte, bookFinanzas]);
 
 
-const bookSaludHumana = new Book ({name:'Salud Humana', author: 'unknown', description: 'here is a description', img: '../img/Portada.png'})
+const bookSaludHumana = new Book ({name:'Salud Humana', author: 'unknown', description: 'here is a description', img: './img/Portada.png'})
 categoryHealth.addRecomendation([bookSaludHumana]);
 
 export {Book, bookSaludHumana, bookProgramar, bookJavascript, bookInvierte, bookFinanzas, bookAprendeIA}
